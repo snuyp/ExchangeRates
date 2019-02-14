8e7de038e4ab660a380df5bce27a9eddef7bf9c9
Dima Selivonchyk <snuyp@yandex,ru>
2019-02-14 16:57:58 +0300

    Create model

---
+++ app/src/main/java/com/example/exchangerates/model/RateShort.kt
@@ -0,0 +1,7 @@
+package com.example.exchangerates.model
+
+class RateShort (
+    var curID: Int?,
+    var date: String?,
+    var curOfficialRate: Float?
+)
---
+++ app/src/main/java/com/example/exchangerates/model/Rates.kt
@@ -0,0 +1,11 @@
+package com.example.exchangerates.model
+
+
+class Rates(
+    var curID: Int?,
+    var date: String?,
+    var curAbbreviation: String?,
+    var curScale: Int?,
+    var curName: String?,
+    var curOfficialRate: Float?
+    )
